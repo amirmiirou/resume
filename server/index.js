2020c739a5de7bea5626ee2862dc442c78f10a47
Your Name <miroua132@gmail.com>
2024-03-19 01:23:48 +0100

    upload

--- server/index.js
+++ server/index.js
@@ -1,22 +1,17 @@
 
 
 const express=require("express")
-
-
-
 const app=express()
-
 const cors=require("cors")
-//const model=require("./schema")
-
-//const mongoose=require("mongoose")
+const model=require("./schema")
+const mongoose=require("mongoose")
 
 
 app.use(cors({origin:"https://resume-client-three.vercel.app"}))
 
 app.use(express.json())
 
-//mongoose.connect("mongodb+srv://miroua132:nait@cluster0.3hythzd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect("mongodb+srv://miroua132:nait@cluster0.3hythzd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 
 app.get("/",(req,res)=>{
@@ -28,28 +23,14 @@ res.send("i am server")
 
 
 app.post("/send",(req,res)=>{
-/*
+
 new model({
 name : req.body.name,
 email : req.body.email,
 phone : req.body.phone,
 adress : req.body.adress,
-message: req.body.message
-
-
-}).save().then((result)=>{
-    res.send(result)
-
-
-
-
-})*/
-
-
-console.log("kkkkkkkkkkkk")
-
-res.send("hello")
-
+message: req.body.message}).save().then((result)=>{
+    res.send(result)})
 })
 
 
